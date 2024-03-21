a71812ef0a5cce25829dd70865d9697bd5f8e71d
meleknasri <150367418+meleknasri@users.noreply.github.com>
2024-03-21 13:27:45 +0100

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import userlist from './userlist';
+
+function App() {
+  return (
+    <div className="App">
+      <UserList />
+    </div>
+  );
+}
+
+export default App;
